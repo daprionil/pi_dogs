56b2fdf1d2dd8f9fe0f528443962d7b838af9389
daprionil <davidarenaoflol23@gmail.com>
2023-05-16 23:46:17 -0500

    Generate models -> [Dog, Temperament]

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,41 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Op } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('Dog', {
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    image:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+      isUrl:true
+    },
     name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true
+    },
+    height:{
       type: DataTypes.STRING,
       allowNull: false,
+      set(value){
+        this.setDataValue('height',`${value}mts`);
+      }
     },
+    weight:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value){
+        this.setDataValue('weight',`${value}kg`);
+      }
+    },
+    yearsOld:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
   });
 };
---
+++ api/src/models/Temperament.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = function(sequelize){
+    return sequelize.define('Temperament',{
+        Id:{
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nombre:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        }
+    })
+}
