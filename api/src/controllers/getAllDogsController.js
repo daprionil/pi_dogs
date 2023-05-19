930d7a372265b6a262d2d86ecae48c85aedc88f5
daprionil <lasparodias60@gmail.com>
2023-05-18 22:12:47 -0500

    Patch getAllUsersController, add API users

--- api/src/controllers/getAllDogsController.js
+++ api/src/controllers/getAllDogsController.js
@@ -1,4 +1,6 @@
+const { default: axios } = require('axios');
 const { Dog, Temperament } = require('../db');
+const { URL_API, API_KEY } = process.env;
 
 const getAllDogsController = async () => {
     const response = await Dog.findAll({
@@ -10,7 +12,41 @@ const getAllDogsController = async () => {
             }
         }
     });
-    return response;
+
+    //! Get values in API
+    const dataAPI = await axios({
+        method:'GET',
+        url:`${URL_API}`,
+        headers:{
+            'x-api-key':API_KEY,
+            'Content-Type':'application/json'
+        }
+    });
+
+    //* Parse Data GET from API
+    const mapDataAPI = dataAPI.data.map(({
+        name,
+        image,
+        id,
+        height,
+        weight,
+        temperament,
+        yearsOld,
+        life_span,
+        Temperaments
+    }) => (
+        {
+            name,
+            image,
+            id,
+            height,
+            weight,
+            Temperaments: temperament ? temperament.split(',')  : Temperaments,
+            yearsOld: yearsOld || life_span.split(' ')[0],
+        }
+    ));
+
+    return [...response, ...mapDataAPI];
 };
 
 module.exports = getAllDogsController;
