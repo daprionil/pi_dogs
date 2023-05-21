051d791d620a343be6544cbd3250e0d2bebf3861
daprionil <davidarenaoflol23@gmail.com>
2023-05-21 01:37:59 -0500

    Add CardDetails

---
+++ client/src/components/CardDogDetail.jsx
@@ -0,0 +1,101 @@
+import { styled } from "styled-components";
+
+function CardDogDetail({image, name, id, height, weight, yearsOld, Temperaments}) {
+    return (
+        <CardDogsStyled>
+            <div className="section_image">
+                <img src={image} alt="" />    
+            </div>
+            <div className="info_dog">
+                <div className="title">
+                    <h1>{name}</h1>
+                    <p><span>#</span>{id}</p>
+                </div>
+                <div className="items_aspect">
+                    <h4>Altura Promedio: <span>{height}</span></h4>
+                    <h4>Peso Promedio: <span>{weight}</span></h4>
+                </div>
+                <h2>Temperamentos</h2>
+                <div className="temperaments">
+                    {
+                        Temperaments.map(({nombre}, i) => (
+                            <Temperament key={i}>{nombre}</Temperament>
+                        ))
+                    }
+                </div>
+            </div>
+        </CardDogsStyled>
+    );
+};
+
+const CardDogsStyled = styled.div`
+    @media (max-width:900px){
+        grid-template-columns: 1fr;
+    }
+
+    width: 100%;
+    max-width: 1000px;
+    min-height: 500px;
+
+    margin: 0 10px;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    
+    box-shadow: 2px 0 5px rgba(0,0,0,0.2);
+    border-radius: 10px;
+    overflow: hidden;
+
+    & .section_image{
+        img{
+            @media (max-width:900px){
+                aspect-ratio: 3/1;
+            }
+            width: 100%;
+            height: 100%;
+
+            object-fit: cover;
+            object-position: 60%;
+            aspect-ratio: 3/2;
+            filter: saturate(1.7);
+        }
+    }
+
+    & .info_dog{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        gap: 10px;
+        
+        padding: 40px 10px;
+        
+        margin-left: 0px;
+        .title{
+            display: flex;
+            align-items: center;
+            justify-content: left;
+            gap: 10px;
+            p{
+                font-size: 1.8rem;
+            }
+        }
+        
+        .temperaments{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: wrap;
+            gap: 10px;
+        }
+    }
+`;
+
+const Temperament = styled.p`
+    background: white;
+    border: 2px solid red;
+    padding: 5px 10px;
+    border-radius: 20px;
+`
+
+export default CardDogDetail;
--- client/src/pages/DogPageDetails.jsx
+++ client/src/pages/DogPageDetails.jsx
@@ -1,13 +1,47 @@
+import { styled } from "styled-components";
 import { useParams } from "react-router-dom";
 import GroupPageDefault from "../components/GroupPageDefault";
+import { useEffect, useState } from "react";
+
+import getADog from "../controllers/getADog";
+import SectionDefaultNullish from '../components/SectionDefaultNullish';
+import Loader from '../base_components/Loader';
+import CardDogDetail from '../components/CardDogDetail';
 
 function DogPageDetails() {
-    const params = useParams();
+    const {idDog} = useParams();
+    const [dog, setDog] = useState({});
+    const [loading, changeLoading] = useState(true);
+
+    useEffect(() => {
+        getADog({id:idDog}).then((dataDog) => {
+            changeLoading(false);
+            setDog(dataDog);
+        });
+    },[])
+
     return (
         <GroupPageDefault>
-            <p>Dog Detail {params.idDog}</p>
+            <DetailsPageDogStyled>
+                {
+                    loading ?
+                        <Loader></Loader>
+                    : dog.hasOwnProperty('name')?
+                        <CardDogDetail {...dog}/>
+                        : <SectionDefaultNullish message="La Raza que Intentas ver no ha sido Encontrada"/>
+                }
+            </DetailsPageDogStyled>
         </GroupPageDefault>
     );
-}
+};
+
+const DetailsPageDogStyled = styled.div`
+    width: 100%;
+    height: 90vh;
+    
+    display:flex;
+    align-items: center;
+    justify-content: center;
+`;
 
 export default DogPageDetails;
