a4a9ed1fc03febcb27efdf00e13fca7189c9c36e
daprionil <davidarenaoflol23@gmail.com>
2023-07-25 23:35:01 -0500

    Fixed logical funcionality to PrivateRouteAuth

--- client/src/components/PrivateAuthRoute.jsx
+++ client/src/components/PrivateAuthRoute.jsx
@@ -1,13 +1,19 @@
 import { useAuthFirebase } from '../context/AuthProvider';
 import { Navigate } from 'react-router-dom';
 
-function PrivateAuthRoute({ children }) {
+function PrivateAuthRoute({ children, loged }) {
     const usuario = useAuthFirebase();
-
+    
     return (
-        usuario ?
-            children
-        : <Navigate to="/log-in" replace/>
+        /* If the route requires the user is not logged in */
+        loged ?
+            usuario ?
+                <Navigate to='/' replace />
+            : children
+        /* If the route requires the user to be logged */
+        : usuario ?
+            children    
+        : <Navigate to="/log-in" replace />
     );
 }
 
