ef632d04233fdd0029d7f2738774d01c11685ad8
daprionil <davidarenaoflol23@gmail.com>
2023-07-25 23:15:49 -0500

    Change size button

--- client/src/base_components/Button.jsx
+++ client/src/base_components/Button.jsx
@@ -2,15 +2,20 @@ import { styled } from "styled-components";
 
 const Button = styled.button`
     @media (max-width: 776px) {
-        font-size: 1.4rem;
+        font-size: 0.9rem;
     }
+    @media (max-width: 470px) {
+        font-size: .7rem;
+    }
+    
     background: ${({bgcolor}) => bgcolor ?? 'white'};
-    font-size: 1.7rem;
+    font-size: 1rem;
     color: ${({color}) => color ?? '#0b0b0b'};
     border-radius: 3px;
     text-align: center;
     padding: 10px 15px;
-    
+    white-space: nowrap;
+
     cursor: pointer;
     
     transition: all .3s ease;
@@ -18,6 +23,7 @@ const Button = styled.button`
         transform: scale(1.01);
         box-shadow: 2px 1px 5px rgba(2,2,2,.1);
     }
+
 `;
 
 export default Button;
