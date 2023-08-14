20c4f7755b3cf5c433cf9a8affa3751c69353e9b
David Santiago Bulla Diaz <lasparodias60@gmail.com>
2023-08-14 12:02:19 -0500

    Estilos de Información ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -7,6 +7,7 @@ import { useAuthFirebase } from "../context/AuthProvider";
 import IconProfile from "../base_components/IconProfile";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ProfilePage = () => {
     const [isEditMode, setIsEditMode] = useState(false);
@@ -15,7 +16,7 @@ const ProfilePage = () => {
         favorite_dogs?.length ?? 0,
         favorite_dogs ? all_dogs.find(({id}) => id === favorite_dogs[0]) ?? {} : {}
     ]);
-
+    
     return (
         <GroupPageDefault>
             <Title>Mi Perfil</Title>
@@ -39,17 +40,25 @@ const ProfilePage = () => {
                                 </ContainerListInfo>
                                 <ListMinInfoContainer>
                                     <ContainerMinInfo>
-                                        <p>Tus Favoritos</p>
+                                        <p>Cantidad de Favoritos</p>
                                         <p>{favoriteCount}</p>
                                     </ContainerMinInfo>
-                                    <ContainerMinInfo>
-                                        <p>Último Agregado</p>
-                                        <p>{lastDog ? lastDog.name : '??'}</p>
-                                    </ContainerMinInfo>
+                                    {
+                                        lastDog &&
+                                        <ContainerMinInfo $infoSpecial>
+                                            <p>Último Agregado</p>
+                                            <p>
+                                                <Link to={`/dogs/${lastDog.id}`} replace>{lastDog.name}</Link>
+                                            </p>
+                                        </ContainerMinInfo>
+                                    }
                                 </ListMinInfoContainer>
                             </>
                     }
                 </Form>
+                <div>
+
+                </div>
             </ContainerProfileInfo>
         </GroupPageDefault>
     );
@@ -59,18 +68,29 @@ const ProfilePage = () => {
 const ContainerProfileInfo = styled.div`
     padding: 10px 15px;
 
+    position: relative;
+    width: fit-content;
     max-width: 1000px;
     margin: 0 auto;
 
     display: flex;
     justify-content: center;
     align-items: center;
+
+    font-size: 1.2rem;
 `;
 
 const ContainerImageProfile = styled.div`
     width: min-content;
     margin: 0 auto;
     filter: drop-shadow(0px 1px 15px rgba(0,0,0,.2));
+
+    transition: all .3s ease-in-out;
+    transform: scale(1);
+    cursor: pointer;
+    &:hover{
+        transform: scale(1.03);
+    }
 `;
 
 const ContainerListInfo = styled.div`
@@ -92,22 +112,32 @@ const ListMinInfoContainer = styled.div`
     justify-content: center;
 `;
 const ContainerMinInfo = styled.div`
-    border-radius: 10px;
-    padding: 10px;
     text-align: center;
-
-    font-weight: 600;
 
     display: flex;
     flex-direction: column;
+    gap: 10px;
 
     & p:nth-child(1){
         filter: drop-shadow(0px 1px 15px rgba(0,0,0,.3));
         font-weight: 600;
     }
     & p:nth-child(2){
-        background: #f0f0f0;
-        box-shadow: 0px 1px 15px rgba(0,0,0,.1);
+        width: fit-content;
+        margin: 0 auto;
+        
+        background: ${({$infoSpecial}) => $infoSpecial ? '#f00000' : '#f0f0f0' };
+        color: ${({$infoSpecial}) => $infoSpecial && '#f0f0f0'};
+        
+        box-shadow: 0px 1px 15px rgba(0,0,0,.2);
+        padding: 4px 7px;
+        border-radius: 3px;
+
+        transition: all .3s ease-in-out;
+        transform: scale(1);
+        &:hover{
+            transform: scale(1.03);
+        }
     }
 `;
 
