9ae07c8f1550a80cd4eea3566b5b19ecf9f733c1
daprionil <davidarenaoflol23@gmail.com>
2023-08-16 20:13:10 -0500

    create alert to success an error situations

---
+++ client/src/utils/alertsSwal.js
@@ -0,0 +1,29 @@
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
+const reactSwalErrorAlert = ({message}) => {
+    //? Show Alert to Denied activity
+    MySwal.fire({
+        title:'Error',
+        icon:'error',
+        text: message,
+        showConfirmButton:false
+    });
+}
+
+const reactSwalSuccessAlert = ({message}) => {
+    //? Show Alert to Denied activity
+    MySwal.fire({
+        title:'Éxito',
+        icon:'success',
+        text: message,
+        showConfirmButton:false
+    });
+}
+
+export {
+    reactSwalErrorAlert,
+    reactSwalSuccessAlert
+}
