574b8305b523616e7f94ec2885e2941275264811
daprionil <davidarenaoflol23@gmail.com>
2023-08-16 20:39:55 -0500

    Integrate UserInputImageProfile component in the ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -3,16 +3,13 @@ import withReactContent from "sweetalert2-react-content";
 import { Title } from "react-head";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import GroupPageDefault from "../components/GroupPageDefault";
 import Form from "../base_components/Form";
 import { useAuthFirebase } from "../context/AuthProvider";
-import IconProfile from "../base_components/IconProfile";
-import Button from "../base_components/Button";
-import InputFileImage from "../base_components/InputFileImage";
-import uploadImageCloudServer from "../controllers/uploadImageCloudServer";
+import UserInputImageProfile from "../components/UserInputImageProfile";
 
 const MySwal = withReactContent(Swal);
 
@@ -23,62 +20,8 @@ const ProfilePage = () => {
         favorite_dogs ? all_dogs.find(({id}) => id === favorite_dogs[0]) ?? {} : {}
     ]);
     const [isEditMode, setIsEditMode] = useState(false);
-    const imageProfile = useRef(null);
     
     //================================================================
-
-
-    //! Change the image in reference
-    const changeImageProfile = (image) => imageProfile.current = image;
-    
-    //! Execute submit for up image to Cloud
-    const handleSubmitChangeImageProfile = async () => {
-        if(imageProfile.current){
-            //! This close window from SweetAlert2
-            uploadImageCloudServer(imageProfile.current).then(console.log);
-            Swal.close();
-            return;
-        }
-
-        //? Show Alert to Denied activity
-        MySwal.fire({
-            title:'Error',
-            icon:'error',
-            text: "No has agregado una imágen",
-            showConfirmButton:false
-        });
-    };
-
-    const handleAlertToChangeImage = async () => {
-        const {isConfirmed} = await MySwal.fire({
-            title: <p>Cambiar Imágen de Perfil</p>,
-            html: <>
-                    <p>¿Te gustaría cambiar tu Imagen de Perfil?</p>
-                    <br />
-                    <InputFileImage setImageState={changeImageProfile} />
-                    <Button
-                        bgcolor="#ff0000"
-                        color="white"
-                        onClick={handleSubmitChangeImageProfile}
-                    >Guardar</Button>
-                </>,
-            showConfirmButton:false,
-        });
-
-        //? If the user was click in the save button
-        if(isConfirmed){
-            //! Validate if the image is valid to change and add Cloudinary
-            console.log(usuario);
-            MySwal.fire({
-                title: 'Se ha cambiado de forma exitosa',
-                icon:"success",
-                html:<>
-                        <Button bgcolor="red" color="white" onClick={Swal.close}>Aceptar</Button>
-                    </>,
-                showConfirmButton:false
-            });
-        }
-    };
 
     return (
         <GroupPageDefault>
@@ -89,13 +32,7 @@ const ProfilePage = () => {
                         isEditMode ?
                             <p>editando...</p>
                         :   <>
-                                <ContainerImageProfile onClick={handleAlertToChangeImage}>
-                                    {
-                                        usuario.photoURL ?
-                                            <img src={usuario.photoURL} alt="" />
-                                        :   <IconProfile $size={'4xl'}/>
-                                    }
-                                </ContainerImageProfile>
+                                <UserInputImageProfile />
                                 <ContainerListInfo>
                                     <p><span>Nombre Usuario: </span>{usuario.displayName ?? 'sin definir'}</p>
                                     <p><span>Correo Electrónico: </span>{usuario.email ?? 'sin definir'}</p>
@@ -141,19 +78,6 @@ const ContainerProfileInfo = styled.div`
     align-items: center;
 
     font-size: 1.2rem;
-`;
-
-const ContainerImageProfile = styled.div`
-    width: min-content;
-    margin: 0 auto;
-    filter: drop-shadow(0px 1px 15px rgba(0,0,0,.2));
-
-    transition: all .3s ease-in-out;
-    transform: scale(1);
-    cursor: pointer;
-    &:hover{
-        transform: scale(1.03);
-    }
 `;
 
 const ContainerListInfo = styled.div`
