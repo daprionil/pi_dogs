5778de9cb087138ca9fc927d34a31ab9164fb481
daprionil <davidarenaoflol23@gmail.com>
2023-09-06 10:58:47 -0500

    profOfConcept git

--- client/src/controllers/updateProfileUserFirebase.js
+++ client/src/controllers/updateProfileUserFirebase.js
@@ -13,7 +13,7 @@ async function updateProfileUserFirebase({username: displayName, emailuser, phon
     const user = auth.currentUser;
 
     //! If doesn't exist an user
-    if(!user) throw new Error('No existe un usuario logeado actualmente para la actualización')
+    if(!user) throw new Error('No existe un usuario logeado actualmente para la actualización de perfil')
     const finalDataUserProfile = Object.entries({displayName, emailuser, phoneNumber}).filter(([,val]) => !!val);
     if(!finalDataUserProfile.length) throw new Error('No hay campos válidos para actualizar el perfil');
     
